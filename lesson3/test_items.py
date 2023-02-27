a87adfd1a9569613217d67544e7a95f214e9ad50
Derthar <dertgar159@gmail.com>
2023-02-27 15:05:51 +0500

    Rename main function

--- lesson3/test_items.py
+++ lesson3/test_items.py
@@ -3,7 +3,7 @@ import time
 import pytest
 
 @pytest.mark.usefixtures
-def test_multi_anguage(browser):
+def test_add_to_bucket_button(browser):
     item = browser.find_elements(By.CSS_SELECTOR, '.btn.btn-lg.btn-primary')
     assert item is not None, 'Кнопка отсутствует на сайте'
     time.sleep(15)
